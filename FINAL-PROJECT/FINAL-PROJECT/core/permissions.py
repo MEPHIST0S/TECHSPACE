0ddcf49421f92cae9e567e4552ff144b2dc26513
Murad Babayev <145931157+MEPHIST0S@users.noreply.github.com>
2024-11-08 03:11:17 +0400

    COMMITED

--- FINAL-PROJECT/FINAL-PROJECT/core/permissions.py
+++ FINAL-PROJECT/FINAL-PROJECT/core/permissions.py
@@ -4,15 +4,13 @@ from rest_framework.permissions import BasePermission, SAFE_METHODS
 
 class IsStaffOrReadOnly(BasePermission):
     def has_permission(self, request, view):
-        # DELETE ONLY for users with True in is_staff
-        if request.method == 'DELETE':
-            return request.user.is_authenticated and request.user.is_staff
-        # SAFE METHODS (like GET) will be allowed for all.
-        return request.method in SAFE_METHODS
-
-    def has_object_permission(self, request, view, obj):
-        # SAFE METHODS (like GET) will be allowed for all.
         if request.method in SAFE_METHODS:
             return True
-        # CHECKING FOR is_staff BEFORE DELETING
-        return request.user.is_staff
+        return request.user.is_authenticated
+
+    def has_object_permission(self, request, view, obj):
+        if request.method in SAFE_METHODS:
+            return True
+        if request.method in ['PUT', 'PATCH']:
+            return request.user.is_authenticated
+        return request.method == 'DELETE' and request.user.is_staff
